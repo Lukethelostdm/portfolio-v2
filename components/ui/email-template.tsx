4a9b0c3426bfc68264807d466b0c2069b3e5cfe8
Lukethelostdm <lukethelostdm@outlook.com>
2024-11-26 14:08:04 +0000

    tidying up email

--- components/ui/email-template.tsx
+++ components/ui/email-template.tsx
@@ -10,7 +10,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   message,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <h1>Hey, {firstName}!</h1>
     <p>Thank you for reaching out to me, I will get back to you as soon as possible.
       In the mean time I have attached a copy of our conversation below for you to review too.
       If you have any questions or concerns at all, you can reach me at luke@lukethewebdev.uk , which is where I will email you from too.
